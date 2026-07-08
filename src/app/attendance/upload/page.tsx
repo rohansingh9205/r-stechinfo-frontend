183215ceeb70731a5c7e0da0d43cc621fb2dd9b8
Rohan <rohansin48@gmail.com>
2026-07-08 20:22:51 +0530

    Fix attendance upload page

---
+++ src/app/attendance/upload/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function UploadAttendancePage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Upload Attendance</h1>
+    </div>
+  );
+}
---
+++ src/app/payroll/templates/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Coming Soon</h1>
+    </div>
+  );
+}
