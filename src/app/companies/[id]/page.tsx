579569c7af9a770545273a8635eb9c2008aaba7b
Rohan <rohansin48@gmail.com>
2026-07-09 19:55:01 +0530

    Fix company profile page

--- src/app/companies/[id]/page.tsx
+++ src/app/companies/[id]/page.tsx
@@ -2,15 +2,11 @@
 
 import AppLayout from "../../../components/layout/app-layout";
 import CompanyProfile from "../../../components/companies/company-profile";
-import { useParams } from "next/navigation";
-
 
 export default function CompanyProfilePage() {
-  const params = useParams();
-
   return (
     <AppLayout>
-      <CompanyProfile id={params.id as string} />
+      <CompanyProfile />
     </AppLayout>
   );
 }
